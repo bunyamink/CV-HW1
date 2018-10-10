85e05ac5bd3fe32c7113398e35891686e14dfe2b
bunyamink <bunyaminibrahim@gmail.com>
2018-10-10 23:08:15 +0300

    menu added

--- hw1.py
+++ hw1.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtCore import QDate, QTime, QDateTime, Qt
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QMenu, QMainWindow, QAction, qApp
+from PyQt5.QtGui import QIcon
 
 
 def main():
@@ -9,7 +10,7 @@ def main():
     sys.exit(app.exec_())
 
 
-class UserInterface(QWidget):
+class UserInterface(QMainWindow):
 
     def __init__(self):
         super().__init__()
@@ -18,9 +19,29 @@ class UserInterface(QWidget):
 
 
     def initUI(self):
-        self.setGeometry(100, 100, 500, 500)
-        self.setWindowTitle('Histogram Equalization')
 
+        openInputAct = QAction('&OpenInput', self)
+        openInputAct.setStatusTip('Open Input')
+        #openInputAct.triggered.connect(qApp.quit)
+
+        openTargetAct = QAction('&OpenTarget', self)
+        openTargetAct.setStatusTip('Open Target')
+        #openTargetAct.triggered.connect(qApp.quit)
+
+        exitAct = QAction('&Exit', self)
+        exitAct.setStatusTip('Exit')
+        exitAct.triggered.connect(qApp.quit)
+
+        self.statusBar()
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(openInputAct)
+        fileMenu.addAction(openTargetAct)
+        fileMenu.addAction(exitAct)
+
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('Histogram Equalization')
         self.show()
 
 if __name__ == '__main__':
